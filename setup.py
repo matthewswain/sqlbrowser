a92e5a164e66248225dba1d87504cf891ba79969
Matthew Swain <matthewswain@gmail.com>
2018-01-16 13:35:17 +0000

    Added minimum python version.

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ from setuptools import setup
 
 setup(
     name="sqlbrowser",
-    version="1.1.1",
+    version="1.1.2",
     packages=["sqlbrowser"],
     author="Matthew Swain",
     author_email="matthewswain@gmail.com",
+    python_requires=">=3.6",
 )
